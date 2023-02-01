6b598925ad3dab88ab6b69215fedfd569291c5ba
Krreesh Gorey <54141356+Krreesh@users.noreply.github.com>
2023-02-01 20:36:03 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to Amazon!";
+		return "Welcome to Amazon Jungle!";
 	}
 
 	public static void main(String[] args) {
