d2f45cb8674869074932b64b00b0755a394d52ec
krreesh <krreesh@outlook.com>
2023-02-01 14:13:20 +0000

    updated src/main/java/com/javatechie/DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Welcome to Jurrasic World!";
 	}
 
 	public static void main(String[] args) {
